f8082f4aec69a88ae59f7c5ec53a92e956a8db90
David <dmhardin102@gmail.com>
2026-04-18 18:48:45 -0700

    reduce polling interval to 1 sec on badge

--- badge/badge.ino
+++ badge/badge.ino
@@ -36,7 +36,7 @@
 U8G2_SH1106_128X64_NONAME_F_HW_I2C oled(U8G2_R0, U8X8_PIN_NONE);
 
 // ---------------- State ------------------------------------------------------
-static const unsigned long POLL_MS = 3000;
+static const unsigned long POLL_MS = 1000;
 static const unsigned long DRAW_MS = 250;
 
 static int     healthScore   = 3;      // 0..3; 3 = calm, 0 = intervention
